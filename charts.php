f08efff147b86af90085877ff4f28d739b7fe681
Filippo <filippo.luppi@gmail.com>
2018-03-05 00:26:55 +0000

    added chats service

--- charts.php
+++ charts.php
@@ -20,18 +20,41 @@ if ($conn->connect_error) {
 if(strcmp($_REQUEST['query'],"carburante")==0)
 {
     
-$sth = mysqli_query($conn,"SELECT  colturas.nome AS name,  ROUND(SUM(attrezzos.costo_carburante*attrezzos.deperimento*attrezzos.consumo*colturas.estensione_ha), 2) AS y FROM Gestione_development.colturas, Gestione_development.trattamentos,Gestione_development.prodotto_trattamentos,Gestione_development.prodottos,Gestione_development.prezzos,Gestione_development.attrezzos WHERE Gestione_development.colturas.id=Gestione_development.trattamentos.id_coltura AND Gestione_development.trattamentos.id=Gestione_development.prodotto_trattamentos.id_trattamento AND Gestione_development.prodotto_trattamentos.id_prodotto=Gestione_development.prodottos.id AND Gestione_development.prodottos.id=Gestione_development.prezzos.id_prodotto AND Gestione_development.trattamentos.id_attrezzo=Gestione_development.attrezzos.id GROUP BY name");
+    
+$sth = mysqli_query($conn,"SELECT  colturas.nome AS label,  ROUND(SUM(attrezzos.costo_carburante*attrezzos.deperimento*attrezzos.consumo*colturas.estensione_ha), 2) AS data FROM Gestione_development.colturas, Gestione_development.trattamentos,Gestione_development.prodotto_trattamentos,Gestione_development.prodottos,Gestione_development.prezzos,Gestione_development.attrezzos WHERE Gestione_development.colturas.id=Gestione_development.trattamentos.id_coltura AND Gestione_development.trattamentos.id=Gestione_development.prodotto_trattamentos.id_trattamento AND Gestione_development.prodotto_trattamentos.id_prodotto=Gestione_development.prodottos.id AND Gestione_development.prodottos.id=Gestione_development.prezzos.id_prodotto AND Gestione_development.trattamentos.id_attrezzo=Gestione_development.attrezzos.id GROUP BY label");
 $rows = array();
+echo '[';
 while($r = mysqli_fetch_assoc($sth)) {
-    $rows[] = $r;
+  
+    echo "{ label: \"";
+    echo $r['label'];
+    echo "\",data: ";
+    echo $r['data'];
+    echo "},";
+}
+echo']';
+
+}
+if(strcmp($_REQUEST['query'],"prodotti")==0)
+{
+    
+    
+$sth = mysqli_query($conn,"SELECT  colturas.nome AS label,  ROUND(SUM((prezzos.prezzo_fattura-(prezzos.prezzo_fattura*prezzos.sconto/100))*prodotto_trattamentos.dose_ha*colturas.estensione_ha),2) AS  data FROM Gestione_development.colturas, Gestione_development.trattamentos,Gestione_development.prodotto_trattamentos,Gestione_development.prodottos,Gestione_development.prezzos,Gestione_development.attrezzos WHERE Gestione_development.colturas.id=Gestione_development.trattamentos.id_coltura AND Gestione_development.trattamentos.id=Gestione_development.prodotto_trattamentos.id_trattamento AND Gestione_development.prodotto_trattamentos.id_prodotto=Gestione_development.prodottos.id AND Gestione_development.prodottos.id=Gestione_development.prezzos.id_prodotto AND Gestione_development.trattamentos.id_attrezzo=Gestione_development.attrezzos.id GROUP BY label");
+$rows = array();
+echo '[';
+while($r = mysqli_fetch_assoc($sth)) {
+  
+    echo "{ label: \"";
+    echo $r['label'];
+    echo "\",data: ";
+    echo $r['data'];
+    echo "},";
+}
+echo']';
+
 }
 
-    header("Content-type:application/json");
-echo  str_replace('"',"'",str_replace('"}',"}",str_replace('"y":"',"y:",str_replace('"name"',"name",json_encode($rows)))));
-}
 
-
-df
 
 
 
